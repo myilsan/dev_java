9558cd3a7d9a96b4183b12949492ed39da6b34bc
최경호 <myilsan72@nave.com>
2024-07-02 16:41:55 +0900

    최조커밋

---
+++ src/main/java/com/dev/dev/BasicController.java
@@ -0,0 +1,26 @@
+package com.dev.dev;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/date")
+    @ResponseBody
+    String hello(){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return "오늘 날짜2 : "+ ZonedDateTime.now().format(formatter);
+
+    }
+
+    @GetMapping("/")
+    String hello2(){
+        return "index.html";
+    }
+}
---
+++ src/main/java/com/dev/dev/DevApplication.java
@@ -0,0 +1,16 @@
+package com.dev.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+			System.out.println("start spring boot!");
+
+
+		}
+
+}
