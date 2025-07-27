a563ede975017df459cc5cee6990401e4f19d552
myilsan <myilsan72@naver.com>
2025-07-28 01:22:57 +0900

    test

--- src/main/java/com/dev/dev/BasicController.java
+++ src/main/java/com/dev/dev/BasicController.java
@@ -31,7 +31,7 @@ public class BasicController<CompanySignupRes> {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        return "오늘 날짜2_branch : "+ ZonedDateTime.now().format(formatter);
+        return "오늘 날짜2_branch22 : "+ ZonedDateTime.now().format(formatter);
 
     }
 
